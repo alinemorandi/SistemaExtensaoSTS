9f63c12530bfed5a9218393e6e9425386ab10194
Aline <alinemorandi90@gmail.com>
2019-03-09 14:23:31 -0300

    alteração da classe sistemasextensaostsapplication

--- src/main/java/br/edu/iftm/extensao/SistemaExtensaoStsApplication.java
+++ src/main/java/br/edu/iftm/extensao/SistemaExtensaoStsApplication.java
@@ -3,6 +3,9 @@ package br.edu.iftm.extensao;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// exercícios da aula de controle de versão do dia 09 de março.
+// alteração da classe 
+
 @SpringBootApplication
 public class SistemaExtensaoStsApplication {
 
